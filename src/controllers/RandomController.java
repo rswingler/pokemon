7ea49c7403cb68913c1d21f7aad9f09c4a0a18f1
Runemark <martin@runemark.com>
2015-04-13 20:53:21 -0600

    Fancy Random

--- src/controllers/RandomController.java
+++ src/controllers/RandomController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -14,27 +15,34 @@ public class RandomController {
 	}
 	
 	public int randomIntBetweenOneAndN(int n) {	
-		try {
+		
+		StringBuffer response = new StringBuffer();
+		try
+		{
+			//URL CONNECTION
+			URL url = new URL("http://www.rollthedice.setgetgo.com/get.php?Sides=" + Integer.toString(n));
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			
-			URL randomURL = new URL("http://www.rollthedice.setgetgo.com/get.php?Sides=" + Integer.toString(n));
-			URLConnection yc = randomURL.openConnection();
-		    BufferedReader in = new BufferedReader(
-		                            new InputStreamReader(
-		                            yc.getInputStream()));
-		    String inputLine;
-
-		    while ((inputLine = in.readLine()) != null) 
-		        System.out.println(inputLine);
-		    in.close();
-		    
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//HEADERS
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json");
+			
+			//GET RESPONSE BODY - LITERALLY JUST A NUMBER WITH NO OTHER WHITESPACE OR CHARACTERS
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null)
+			{
+				response.append(inputLine);
+			}
+			in.close();
+			
+			String responseString = response.toString();
+			return Integer.parseInt(responseString);
 		}
-	    
-	    return 0;
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return -1;
+		}
 	}
 }
